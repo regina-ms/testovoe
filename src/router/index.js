5bea1ff5280df3f61c4b03a1cbce5d6f13cd0086
sevastyan <foxhable@gmail.com>
2023-08-15 19:11:51 +0300

    refactor: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,57 @@
 import appConstants from '../common/constants.js';
 import cartPage from '../pages/cartPage.js';
 import mainPage from '../pages/mainPage.js';
-import createMainPage from '../pages/createMainPage.js';
-import openBrands from '../api/openBrands.js';
-import getArrayOfItems from '../api/getArrayOfItems.js';
-import appendPagNumbBtn from '../components/renderPaginationBtns.js';
-import showItems from '../components/showItems.js';
 
+/*
+    Вместо if условий или блока switch case я бы создал подобный объект со значением
+    { [название пути]: функция_для_рендера }
+ */
+const routes = {
+    // Не совсем понимаю зачем resize обработчик
+    [appConstants.pathnames.mainPage]: mainPage,
+    [appConstants.pathnames.cartPage]: cartPage
+}
 
-function render(path) {
-    if(path === appConstants.pathnames.mainPage) {
-        mainPage();
-        window.addEventListener("resize", () => {
-            app.innerHTML = createMainPage();
-            openBrands();
-            const products = getArrayOfItems("item");
-            appendPagNumbBtn(products);
-            showItems(products, appConstants.itemsPerPage, appConstants.currentPageDefault);
-        });
-    }
-    if(path === appConstants.pathnames.cartPage) {
-        cartPage();
+function error404Page() {
+    document.body.innerHTML = '<h1>Ошибка 404 - страница не найдена</h1>'
+}
+
+function error500Page() {
+    document.body.innerHTML = '<h1>Ошибка 500 при рендере</h1>'
+}
+
+function renderPath(path) {
+    // С текущим роутером есть проблема - он ломается при несуществующем пути
+    // Роутинг через объект позволяет делать удобную проверку на существование через ключевое слово in
+    if (!(path in routes)) return error404Page()
+
+    try {
+        routes[path]()
+    } catch (err) {
+        // Так же никогда не стоит забывать про обработку ошибок
+        error500Page()
+        console.error(err)
     }
 }
 
 export function goTo(path) {
     window.history.pushState({path}, "path", path);
-    render(path);
+    renderPath(path)
 }
 
 export default function initRoute() {
-    window.addEventListener('popstate', e => {
-        render(new URL(window.location.href).pathname);
-    })
+    // У window.location есть параметр pathname, создавать URL из href - лишнее
+    window.addEventListener('popstate', () => renderPath(window.location.pathname))
+
     document.querySelectorAll('[href^="/"]').forEach(el => {
         el.addEventListener('click', e => {
             e.preventDefault();
-            const {pathname:path} = new URL(e.target.href);
-            goTo(path);
+            // Читабельнее без деструктуризации.
+            // Лучше читать event.currentTarget, т.к. если внутри ссылки будут другие элементы,
+            // то из-за всплытия target'ом будут они, а у них href'а не будет
+            goTo(new URL(e.currentTarget.href).pathname);
         })
     })
-    render(new URL(window.location.href).pathname);
+
+    renderPath(window.location.pathname)
 }
